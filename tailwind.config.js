bc5c3631ea8ea3cbd3704216e2c5efa803481101
junghun-98 <cjswpwjdgns@naver.com>
2021-11-07 17:10:05 +0900

    홈페이지 1차(페이지 설명)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+        'kw': "url('https://www.kw.ac.kr/ko/img/ideology02_01.jpg')",
+    },
   },
   variants: {
     extend: {},
