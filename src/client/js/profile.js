2f42e01ef4c4cc282c8486a7cc0c40c76e692b11
junghun-98 <cjswpwjdgns@naver.com>
2021-11-14 17:23:17 +0900

    프로필 수정 기능 완성

--- src/client/js/profile.js
+++ src/client/js/profile.js
@@ -7,20 +7,34 @@ import { createEditor } from "./editor";
 import {
   deleteImgFromFirebase,
   getUser,
+  getUserByUid,
   isLoggedIn,
   updateUser,
   uploadImg,
 } from "./utils";
 import { authService } from "./firebase";
 import { async } from 'regenerator-runtime';
+import { assert } from '@firebase/util';
+
+
+const hrefParsed = window.location.href.split("/");
+const profileId = hrefParsed[hrefParsed.length - 1];
 
 const updateButton = document.getElementById("updateButton");
 const container = document.getElementById("container");
+
+const userName = document.getElementById("userName");
+const userEmail = document.getElementById("userEmail");
+
+const img = document.getElementById("profileImage");
+
+//프로필 수정버튼 클릭시 생성될 인터페이스
+
 const updateDisplay =
 "<div id = 'update'>"
 + "<div id = 'profileImg'>"
   + "<label>"
-    + "<input class = 'hidden'"
+    + "<input class = 'w-52 h-8 hidden mx-auto my-2'"
       + "type='file'"
       + "name='fileUploader'"
       + "id = 'profileUploader'"
@@ -35,31 +49,84 @@ const updateDisplay =
 + "</div>"
 + "</div>";
 
+const updateDisplayName = async () => {
+  const user = await getUser();
+  let newName = null;
+  const displayName = document.getElementById('displayName');
 
-const chageImage = () => {
-  const uploader = document.getElementById("profileUploader");
-  const img = document.getElementById("profileImage");
-
-  uploader.addEventListener("change", async (e) => {
-    const file = e.target.files[0];
-    console.log(file);
-    try{
-      const url = await uploadImg(file);
-      if(url){
-        if(img && img.tagName === "IMG") {
-          img.src = url;
-          return url;
-        }
-      }
-    }
-    catch (error){
-      console.log(error);
-      alert(error);
-    }
-  });
+  if(displayName != null){
+    newName = displayName.value;
+    updateUser(user.uid, { displayName: newName, });
+  }
 }
 
-//프로필 수정시 바뀔 화면
+//비밀번호 업데이트
+const updatePasswd = async () => {
+  const user = await getUser();
+
+  const passwdFirst = document.getElementById('passwdFirst');
+  const passwdSecond = document.getElementById('passwdSecond');
+
+  if(passwdFirst == null && passwdSecond == null) { return; }
+
+  let newPasswd1 = passwdFirst.value;
+  let newPasswd2 = passwdSecond.value;
+
+  console.log(newPasswd1);
+  console.log(newPasswd2);
+
+  if(newPasswd1 === '' && newPasswd2 === '') { return; }
+
+  else if(newPasswd1 === newPasswd2){
+    const { ok, error } = await updateUser(user.uid, 
+      { password: newPasswd2, });
+
+    if(!ok || error){
+      console.log("requires-recent-login");
+      alert(error);
+      await authService.signOut();
+      window.location.reload;
+    }
+    else{
+      console.log("checkout firebase");
+    }
+    alert("비밀번호가 변경되었습니다. 다시 로그인해주세요.");  
+  }
+
+  else{
+    alert("비밀번호가 다릅니다.");
+  }
+}
+
+const updateImage = async () => {
+  const user = await getUser();
+
+  const uploader = document.getElementById("profileUploader");
+
+  console.log(uploader);
+  if(uploader){
+    uploader.addEventListener("change", async (e) => {
+      const file = e.target.files[0];
+      console.log(file);
+      try{
+        const url = await uploadImg(file);
+        if(url){
+          if(img && img.tagName === "IMG") {
+            img.src = url;
+            console.log(url);
+            updateUser(user.uid, { photoURL: url, });
+          }
+        }
+      }
+      catch (error){
+        console.log(error);
+        alert(error);
+      }
+    });
+  }
+}
+
+//프로필 수정버튼 클릭시 바뀔 화면 & update
 const changeInfoDisplay = async () => {
   
   if(updateButton.innerText === '프로필 수정'){
@@ -67,26 +134,22 @@ const changeInfoDisplay = async () => {
     container.insertAdjacentHTML('beforeend', updateDisplay);
   }
   else{
-    updateButton.innerText = '프로필 수정'
-    const insertedContent = document.getElementById("update");
-    if(insertedContent) {
-      insertedContent.parentNode.removeChild(insertedContent);
-    }
+    updateDisplayName();
+    updatePasswd();
+    alert('닉네임이 변경되었습니다.');
   }
 }
 
 const clickUpdateButton = async () => {
   try {
     const user = await getUser();
-    const hrefParsed = window.location.href.split("/");
-    const profileId = hrefParsed[hrefParsed.length - 1];
-    //const updateData = { newPasswd, newphotoURL, newDisplayName };
     
     //uid 판별을 위한 호출
     const { error, ok } = await updateUser(user.uid , {});
     
     if (ok) {
       changeInfoDisplay();
+      updateImage();
     } else {
       alert("프로필을 수정할 수 없습니다.");
       alert(error);
@@ -96,6 +159,20 @@ const clickUpdateButton = async () => {
   }
 }
 
+const showUserInfo = async () => {
+  const profileUser = await getUserByUid(profileId);
+  
+  img.src = profileUser.photoURL;
+  
+  if(profileUser.displayName == null){
+    userName.innerText = '설정된 닉네임이 없습니다.';
+  }
+  else{
+    userName.innerText = 'User Display Name : ' + profileUser.displayName;
+  }
+  userEmail.innerText = 'User Email : ' + profileUser.email;
+}
+
 const init = async () => {
   document.body.hidden = false;
   updateMenuBar();
@@ -103,6 +180,8 @@ const init = async () => {
   if (updateButton) {
     updateButton.addEventListener("click", clickUpdateButton);
   }
+
+  showUserInfo();
 };
 
 const preload = () => {
